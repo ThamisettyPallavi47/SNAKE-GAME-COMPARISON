224aa7436ed9b764adc52667ed3dbc3f917fda55
ThamisettyPallavi47 <pallavithammisetty@gmail.com>
2026-02-28 12:36:20 +0530

    Prepared backend for Render deployment

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from ai import get_next_move
-
+import os
 app = Flask(__name__)
 CORS(app)
 
@@ -25,5 +25,14 @@ def next_move_astar():
     return jsonify({"move": move})
 
 
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
